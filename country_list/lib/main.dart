5f29b116139572bf163c1299a670366b6cba15c2
Tanweer Anwar <tanweer.anwar919@gmail.com>
2021-06-03 23:46:11 +0530

    Improve structure of country_list project

--- country_list/lib/main.dart
+++ country_list/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/HomeScreen.dart';
 void main() {
   runApp(
     MaterialApp(
@@ -6,63 +7,4 @@ void main() {
       home: HomePage(),
     ),
   );
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('First App')
-      ),
-      body: CountryList(),
-    );
-  }
-}
-
-
-class CountryList extends StatelessWidget {
-  final List<String> countries = [ "Algeria", "Angola", "Benin", "Botswana", "Burkina Faso", "Burundi", "Cabo Verde", "Cameroon", "Chad", 
-  "Comoros ", "Congo (the)", "Côte d'Ivoire", "Djibouti", "Egypt", "Equatorial Guinea", "Eritrea", "Ethiopia", 
-  "Gabon", "Gambia ", "Ghana", "Guinea", "Guinea-Bissau", "Kenya", "Lesotho", "Liberia", "Libya", "Madagascar", "Malawi", "Mali"];
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: countries.length,
-      itemBuilder: (BuildContext context, int index) => Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 2.0),
-        child: Card(
-          child: Container(
-            height: 70,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  CircleAvatar(
-                    backgroundColor: Color(
-                      (Random().nextDouble() * 0xFFFFFF).toInt(),
-                    ).withOpacity(1.0),
-                    child: Text(
-                      countries[index].substring(0, 2),
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: Text(
-                        countries[index],
-                        style: TextStyle(fontSize: 18),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
 }
